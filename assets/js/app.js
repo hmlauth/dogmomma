461345ec62e4c0c04c2ce0136b5d7690fb15f1e7
Andromeda <hannalauth@gmail.com>
2019-09-19 21:46:39 -0700

    pulled info from form on click of submit

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,24 @@ $(document).ready(() => {
     // Scrollspy
     $('body').scrollspy({ target: '#navbar-link' });
 
-    console.log(window);
+    $('#submit').on('click', () => {
+        event.preventDefault();
+        let name = $('#name').val().trim();
+        let email = $('#email').val().trim();
+        let phone = $('#phone').val().trim();
+        let animalName = $('#animal-name').val().trim();
+        let message = $('#message').val().trim();
+
+        let contactMessage = {
+            name, 
+            email, 
+            phone, 
+            animalName, 
+            message
+        };
+
+        console.log(contactMessage);
+
+    })
 
 })
