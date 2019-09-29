6fa932e012726ff968f52962f6fa6a4d817aff39
Andromeda <hannalauth@gmail.com>
2019-09-29 15:22:34 -0700

    added html route for load get request

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const contactRoutes = require('./routes/contactRoutes.js');
+const htmlRoutes = require('./routes/htmlRoutes.js');
 
 app.use(bodyParser.json()); // parse form inputs into json
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(__dirname + 'index.html'));
+app.use(express.static(__dirname + '/public'));
 
+htmlRoutes(app);
 contactRoutes(app);
 
 const PORT = process.env.PORT || 3001;
